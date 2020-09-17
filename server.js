4db9ce143716fb5bc205c82168ac39786c9a50ac
panjigemilang <41560479+panjigemilang@users.noreply.github.com>
2020-09-17 14:03:13 +0700

    hapus git 2

--- server.js
+++ server.js
@@ -20,31 +20,34 @@ io.on("connection", (socket) => {
   console.log("User connected : ", connections.length)
 
   // Events
-  socket.on("login", ({ name, isLoggedIn }, callback) => {
-    socket.username = data
+  socket.on("login", ({ name }, callback) => {
+    socket.username = name
 
     if (users.indexOf(socket.username) > -1) {
       callback(false)
     } else {
       callback(true)
-      connections.push(name)
       users.push(socket.username)
       updateUsernames()
 
       console.log("Users connected : ", users)
       if (Object.keys(users).length == 2) {
-        io.emit("connected", socket.username)
         io.emit("game-start")
         io.emit("user-connected", { name, users: connections.length })
       }
     }
   })
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id])
-    connections.splice(connections.indexOf(users[socket.id], 1))
-    console.log("Disconnected user : ", [users[socket.id], connections])
+  socket.on("get-user-connected", ({ name }) => {
+    // const index = users.indexOf(socket.username, 1)
+    io.emit("get-user-connect", { name: socket.username })
+  })
 
-    delete users[socket.id]
+  socket.on("disconnect", () => {
+    connections.splice(connections.indexOf(socket, 1))
+    users.splice(users.indexOf(socket.username), 1)
+    updateUsernames()
+    socket.broadcast.emit("user-disconnected", socket.username)
+    console.log("Disconnected user : ", [socket.username, connections])
   })
 })
